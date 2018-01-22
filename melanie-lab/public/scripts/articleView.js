856dfea176d1bcef768aaf9afdd03cc0cd0989ce
Melanie Cohen <melaniebcohen@gmail.com>
2018-01-22 14:17:07 -0800

    html sites loading properly

--- melanie-lab/public/scripts/articleView.js
+++ melanie-lab/public/scripts/articleView.js
@@ -134,7 +134,7 @@ var app = app || {};
     let template = Handlebars.compile($('#author-template').text());
 
     // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
-    // app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
+    app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
 
     // REVIEW: Simply write the correct values to the page:
     $('#blog-stats .articles').text(app.Article.all.length);
